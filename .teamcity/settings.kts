4d317c4659d2c369afd135b854482574ab4e692b
Cory Knox <me@coryknox.dev>
2026-06-09 15:41:33 -0700

    Setup TeamCity

---
+++ .teamcity/settings.kts
@@ -0,0 +1,60 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
+import jetbrains.buildServer.configs.kotlin.v2019_2.Dependencies
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.PullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.ScheduleTrigger
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.schedule
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+
+project {
+    buildType(CorbobTest)
+}
+
+object CorbobTest : BuildType({
+    id = AbsoluteId("CorbobTest")
+    name = "Corbob-Test"
+
+    artifactRules = """
+    """.trimIndent()
+
+    params {
+        param("env.vcsroot.branch", "%vcsroot.branch%")
+        param("env.Git_Branch", "%teamcity.build.vcs.branch.TeamCityPRTesting_VcsRoot%")
+        param("teamcity.git.fetchAllHeads", "true")
+        password("env.GITHUB_PAT", "%system.GitHubPAT%", display = ParameterDisplay.HIDDEN, readOnly = true)
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+
+        branchFilter = """
+            +:*
+        """.trimIndent()
+    }
+
+    steps {
+        powerShell {
+            name = "Prerequisites"
+            scriptMode = script {
+                content = """
+                    whoami
+                """.trimIndent()
+            }
+        }
+    }
+
+    triggers {
+        vcs {
+            branchFilter = """
+                +:*
+                -:master
+                -:support/*
+            """.trimIndent()
+        }
+    }
+
+})
